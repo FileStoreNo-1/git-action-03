9e688cd3108101d9d8c28d8578e6423e3d158f97
landmarkC30testing <ifeanyibenard2@gmail.com>
2023-04-13 12:50:42 -0700

    new workflow

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" { 
+  region  = "us-west-2"
+}
+resource "aws_vpc" "terravpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+     Name = "myvpc"
+  }
+}
